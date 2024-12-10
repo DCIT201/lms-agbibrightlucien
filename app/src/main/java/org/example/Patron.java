df1aaad219963ee8ec58da6dfaf375b8bdaa1139
Bright Lucien Jnr Agbi <agbibrightlucienjnr@gmail.com>
2024-12-10 17:21:19 +0000

    Update Patron.java

--- app/src/main/java/org/example/Patron.java
+++ app/src/main/java/org/example/Patron.java
@@ -1,39 +1,24 @@
 package org.example;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Patron {
     private String name;
     private String id;
-    private List<Book> borrowedBooks;
 
     public Patron(String name, String id) {
         this.name = name;
         this.id = id;
-        this.borrowedBooks = new ArrayList<>();
     }
 
+    // Getters
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public List<Book> getBorrowedBooks() {
-        return borrowedBooks;
-    }
-
-    public void setBorrowedBooks(List<Book> borrowedBooks) {
-        this.borrowedBooks = borrowedBooks;
+    @Override
+    public String toString() {
+        return "Patron [Name=" + name + ", ID=" + id + "]";
     }
 }
